c3cecef88b96aad9c83ed1ce2a001eded92b2af3
tony-dev-wm <ta.devopsuk@gmail.com>
2023-05-26 20:49:27 +0100

    update

---
+++ src/components/Edit.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Edit() {
+  
+  return (
+  <form></form>);
+}
+
+export default Edit;
+
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useState } from "react";
+import Category from "./Categories";
+import Menu from "./Menu";
+import items from "../data.json";
+
+
+const Home = () => {
+    const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+  const [menuItems, setMenuItems] = useState(items);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <div className="App">
+      <h2>
+        <u>Our Menu</u>
+      </h2>
+      <Category categories={allCategories} filterItems={filterItems} />
+      <Menu items={menuItems} />
+    </div>
+   
+  )
+}
+
+export default Home
