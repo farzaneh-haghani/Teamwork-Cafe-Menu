07224c98b86ea54e49e432bd7a42152db440a77c
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-05-29 17:24:04 +0100

    error solved

--- client/src/components/AdminMenu.jsx
+++ client/src/components/AdminMenu.jsx
@@ -1,31 +1,27 @@
-
 import React, { useState, useContext } from "react";
-import items from "../data.json";
+import dataJson from "../data.json";
 import { Link } from "react-router-dom";
 import AppContext from "./Context";
 import SearchItem from "./SearcItem";
 
 const AdminMenu = () => {
-  const { setId } = useContext(AppContext);
+  const { menuData, setMenuData, setId } = useContext(AppContext);
+  const [activeRow, setActiveRow] = useState(null);
 
   const editHandler = (id) => {
     setId(id);
   };
 
-const AdminMenu = ({menuData, setMenuData, fetchedData}) => {
-    let [activeRow, setActiveRow] = useState(null);
-
-    function handleClick(rowId) {
-        setActiveRow(rowId === activeRow ? null : rowId);
-      }
+  function handleClick(rowId) {
+    setActiveRow(rowId === activeRow ? null : rowId);
+  }
 
   return (
     <div>
-      <SearchItem setMenuData={setMenuData}/>
+      <SearchItem />
       <h4>Menu Items</h4>
-      <button onClick={() => setMenuData(fetchedData)}>All</button>
+      <button onClick={() => setMenuData(dataJson)}>All</button>
       <table>
- { id, title, desc, price, img } = menuItem;
         <thead>
           <tr>
             <th className="table-titles">Id</th>
@@ -38,23 +34,30 @@ const AdminMenu = ({menuData, setMenuData, fetchedData}) => {
             <th className="table-titles">Delete</th>
           </tr>
         </thead>
+
         <tbody>
           {menuData?.map((menuItem) => {
-           console.log(menuItem.id);
+            const { id, title, category, desc, price, img } = menuItem;
             return (
-              <tr key={menuItem.id}
-                onClick={() => handleClick(menuItem.id)}
+              <tr
+                key={id}
+                onClick={() => handleClick(id)}
                 style={{
-                  backgroundColor: menuItem.id === activeRow ? "#c2c1c6" : "#f7f7f7"
+                  backgroundColor:
+                    menuItem.id === activeRow ? "#c2c1c6" : "#f7f7f7",
                 }}
               >
-                <td>{menuItem.id}</td>
-                <td>{menuItem.title}</td>
-                <td>{menuItem.category}</td>
-                <td>{menuItem.price}</td>
-                <td>{menuItem.img}</td>
-                <td>{menuItem.desc}</td>
-                <td><Link to ="/editForm"> <button onClick={() => editHandler(id)}>Edit</button></Link></td>
+                <td>{id}</td>
+                <td>{title}</td>
+                <td>{category}</td>
+                <td>{price}</td>
+                <td>{img}</td>
+                <td>{desc}</td>
+                <td>
+                  <Link to="/editForm">
+                    <button onClick={() => editHandler(id)}>Edit</button>
+                  </Link>
+                </td>
                 <td>
                   <button>Delete</button>
                 </td>
@@ -65,7 +68,6 @@ const AdminMenu = ({menuData, setMenuData, fetchedData}) => {
       </table>
     </div>
   );
-}
+};
 
 export default AdminMenu;
-
--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const data = require("./data.json");
+const menuData = require("./data.json");
+
+const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -11,27 +12,28 @@ app.use(cors({ origin: true, credentials: true }));
 
 app.get("/", (req, res) => res.status(200).json({ data: data }));
 
+
 app.put("/editForm", (req, res) => {
   const id = parseInt(req.body.id);
   const newItem = req.body;
-  const itemIndex = data.findIndex((item) => item.id === id);
+  const itemIndex = menuData.findIndex((item) => item.id === id);
   if (itemIndex === -1) {
     res
       .status(400)
       .json({ success: "failure", message: "This id does not exist" });
   }
-  data.splice(itemIndex, 1, newItem);
+  menuData.splice(itemIndex, 1, newItem);
   res.status(200).json({ success: true, item: newItem });
 });
-const port = process.env.PORT ?? 3005;
 
 
 app.post("/", (req, res) => {
   res.json({ success: true });
 });
 
+
 app.get("/menu/q", function (request, response) {
-  const str = request.query.str;
+  const str = (request.query.str).toLowerCase();
 
   const filteredMenuItems = menuData.filter((item) => {
     return (
@@ -48,6 +50,8 @@ app.get("/menu/q", function (request, response) {
   }
 });
 
+
+const port = process.env.PORT ?? 3005;
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:3005`)
 );
